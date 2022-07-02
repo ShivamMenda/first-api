a74b7bf3bd12355b361e921bbd3a15e026b5d626
Shivam Menda <shivammenda2002@gmail.com>
2022-07-02 18:15:06 +0530

    Create and read operations.

--- blog/main.py
+++ blog/main.py
@@ -1,13 +1,41 @@
+from hashlib import new
+from turtle import title
 from . import schemas
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends,status,Response,HTTPException
 from . import models 
-from .database import engine
+from .database import SessionLocal, engine
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
+
+import blog
 
 app=FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
-@app.post('/blog')
-def create(request:schemas.Blog):
-    return request
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post('/blog',status_code=status.HTTP_201_CREATED) #Create
+def create(request:schemas.Blog,db: Session=Depends(get_db)):
+    new_blog=models.Blog(title=request.title,body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+@app.get('/blog') #Read
+def all(db: Session=Depends(get_db)):
+    blogs=db.query(models.Blog).all()
+    return blogs
+
+@app.get('/blog/{id}',status_code=200) #Read by id
+def show(id,response:Response,db: Session=Depends(get_db)):
+    blogs=db.query(models.Blog).filter(models.Blog.id==id).first()
+    if not blogs:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    return blogs
+
