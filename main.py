fdca2336890b56e1b3fc0aafab61f148012efca1
Shivam Menda <shivammenda2002@gmail.com>
2022-07-02 13:40:37 +0530

    Learnt about POST Requests and BaseModel

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from cgitb import text
 import imp
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
+import uvicorn
 
 app=FastAPI() 
 #To access docs add /docs to access Swagger UI
@@ -24,3 +27,17 @@ def show(id: int): #Type matching with Pydantic under the hood.
 @app.get('/blog/{id}/comments') #Path params
 def comment(id):
     return {'data':{'1','2'}}
+
+class Blog(BaseModel): #BaseModel 
+    title:str
+    body:str
+    published:Optional[bool]
+
+
+@app.post('/blog') #post request with pydantic model
+def create_blog(request:Blog):
+    return{'data':f"Blog is created with title {request.title}"}
+
+
+# if __name__=="__main__":  #To change ports
+#     uvicorn.run(app,host="127.0.0.1",port=5500)
