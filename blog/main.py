2f884daede0680c5da2a0b200981f66e03fcc13a
Shivam Menda <shivammenda2002@gmail.com>
2022-07-03 12:31:44 +0530

    CRUD operations implemented

--- blog/main.py
+++ blog/main.py
@@ -1,5 +1,7 @@
 from hashlib import new
 from turtle import title
+
+from sqlalchemy import false
 from . import schemas
 from fastapi import FastAPI,Depends,status,Response,HTTPException
 from . import models 
@@ -26,6 +28,26 @@ def create(request:schemas.Blog,db: Session=Depends(get_db)):
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT) #Delete
+def destroy(id,db: Session=Depends(get_db)):
+    blog=db.query(models.Blog).filter(models.Blog.id==id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with {id} not found")
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return {'done'}
+
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
+def update(id,request:schemas.Blog,db: Session=Depends(get_db)):
+    blog=db.query(models.Blog).filter(models.Blog.id==id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with {id} not found")
+    blog.update(request.dict())
+    db.commit()
+    return 'updated successfully'
+
+    
 
 @app.get('/blog') #Read
 def all(db: Session=Depends(get_db)):
