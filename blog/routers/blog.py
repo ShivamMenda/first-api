938684fb74369e60b8bae5188c48021bd7b64d49
Shivam Menda <shivammenda2002@gmail.com>
2022-07-10 23:01:57 +0530

    Auth with JWT tokens complete.

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter,Depends,status,HTTPException,Response
+from blog.oauth2 import get_current_user
 from .. import schemas,models
 from typing import List
 from .. import database
@@ -10,12 +11,12 @@ router=APIRouter(
 )
 
 @router.get('/',response_model=List[schemas.ShowBlog]) #Read
-def all(db: Session=Depends(database.get_db)):
+def all(db: Session=Depends(database.get_db),current_user: schemas.User=Depends(get_current_user)):
     blogs=db.query(models.Blog).all()
     return blogs
 
 @router.post('/',status_code=status.HTTP_201_CREATED) #Create
-def create(request:schemas.Blog,db: Session=Depends(get_db)):
+def create(request:schemas.Blog,db: Session=Depends(get_db),current_user: schemas.User=Depends(get_current_user)):
     new_blog=models.Blog(title=request.title,body=request.body,user_id=1)
     db.add(new_blog)
     db.commit()
@@ -23,7 +24,7 @@ def create(request:schemas.Blog,db: Session=Depends(get_db)):
     return new_blog
 
 @router.delete('/{id}',status_code=status.HTTP_204_NO_CONTENT) #Delete
-def destroy(id,db: Session=Depends(get_db)):
+def destroy(id,db: Session=Depends(get_db),current_user: schemas.User=Depends(get_current_user)):
     blog=db.query(models.Blog).filter(models.Blog.id==id)
     if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with {id} not found")
@@ -32,7 +33,7 @@ def destroy(id,db: Session=Depends(get_db)):
     return {'done'}
 
 @router.put('/{id}',status_code=status.HTTP_202_ACCEPTED) #Update
-def update(id,request:schemas.Blog,db: Session=Depends(get_db)):
+def update(id,request:schemas.Blog,db: Session=Depends(get_db),current_user: schemas.User=Depends(get_current_user)):
     blog=db.query(models.Blog).filter(models.Blog.id==id)
     if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with {id} not found")
@@ -41,7 +42,7 @@ def update(id,request:schemas.Blog,db: Session=Depends(get_db)):
     return 'updated successfully'
 
 @router.get('/{id}',status_code=200,response_model=schemas.ShowBlog) #Read by id
-def show(id,db: Session=Depends(get_db)):
+def show(id,db: Session=Depends(get_db),current_user: schemas.User=Depends(get_current_user)):
     blogs=db.query(models.Blog).filter(models.Blog.id==id).first()
     if not blogs:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
--- blog/token.py
+++ blog/token.py
@@ -1,7 +1,12 @@
 from datetime import datetime, timedelta
+
+
 from typing import Optional
 
 from jose import JWTError,jwt
+from . import schemas
+
+from blog.schemas import TokenData
 
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
@@ -14,3 +19,13 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+def verify_token(data:str,credentials_exception):
+    try:
+        payload = jwt.decode(data, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(email=username)
+    except JWTError:
+        raise credentials_exception
